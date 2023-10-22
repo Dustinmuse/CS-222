ed878ed80fe5c83be0f96bbb20f433149f19e1fb
Dustin Muse <Dustin.muse12@gmail.com>
2023-10-22 16:52:16 -0400

    plot practice

---
+++ plot.py
@@ -0,0 +1,13 @@
+import matplotlib as plt
+
+x = ['Q1', 'Q2', 'Q3', 'Q4']
+y = [10000, 12000, 11000, 8000]
+plt.xlabel('Quarter')
+plt.ylabel('Sales')
+plt.title('Sales by quarter')
+#creates bar chart
+#plt.bar(x, y)
+
+#creates a line chart and makes it a double dashed line
+plt.plot(x, y, marker = 'o', color = 'yellow', linestyle = '--')
+plt.show()
